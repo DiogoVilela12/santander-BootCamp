fda88092ed17501a4d612cca140ddbf19dcf65f1
Diogo Vilela <diogovillela12@gmail.com>
2023-10-26 15:14:47 -0300

    Aplicando responsividade na home page

--- src/app/pages/home/components/card-text/card-text.component.ts
+++ src/app/pages/home/components/card-text/card-text.component.ts
@@ -1,15 +1,20 @@
-import { Component, Input } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-text',
   templateUrl: './card-text.component.html',
   styleUrls: ['./card-text.component.css']
 })
-export class CardTextComponent {
+export class CardTextComponent implements OnInit {
   @Input() urlImagem: string = '';
   @Input() urlLink: string = '';
   @Input() linkExterno:boolean = false;
   @Input() reverse: string = 'row';
   @Input() bgColor: string = '#e0fcff'
+
+  ngOnInit() {
+    if (window.screen.width <= 768) {
+      this.reverse = 'column-reverse'
+    }
+  }
 }
